5e7224a4b48e6f9e26558cc36ef42461683f1740
Cassivs Gabriellis <cassiogabrielcontato@gmail.com>
2023-06-10 16:31:28 -0300

    added more HTTP methods

--- main.go
+++ main.go
@@ -5,61 +5,150 @@ import (
 	"net/http"
 )
 
-// album represents data about a record album.
-type album struct {
+// Album represents data about a record album.
+type Album struct {
 	ID     string  `json:"id"`
 	Title  string  `json:"title"`
 	Artist string  `json:"artist"`
 	Price  float64 `json:"price"`
 }
 
+// Create a new struct to handle API responses
+type APIResponse struct {
+	Success bool        `json:"success"`
+	Data    interface{} `json:"data"`
+	Error   string      `json:"error,omitempty"`
+}
+
 // albums slice to seed record album data.
-var albums = []album{
+var albums = []Album{
 	{ID: "1", Title: "Blue Train", Artist: "John Coltrane", Price: 56.99},
 	{ID: "2", Title: "Jeru", Artist: "Gerry Mulligan", Price: 17.99},
 	{ID: "3", Title: "Sarah Vaughan and Clifford Brown", Artist: "Sarah Vaughan", Price: 39.99},
 }
 
-// getAlbums responds with the list of all albums as JSON.
-func getAlbums(c *gin.Context) {
-	c.IndentedJSON(http.StatusOK, albums)
+// GetAlbums responds with the list of all albums as JSON.
+func GetAlbums(c *gin.Context) {
+	response := APIResponse{
+		Success: true,
+		Data:    albums,
+	}
+	c.JSON(http.StatusOK, response)
 }
 
-// postAlbums adds an album from JSON received in the request body.
-func postAlbums(c *gin.Context) {
-	var newAlbum album
+// GetAlbumByID locates the album whose ID value matches the id
+// parameter sent by the client, then returns that album as a response.
+func GetAlbumByID(c *gin.Context) {
+	id := c.Param("id")
 
-	// Call BindJSON to bind the received JSON to newAlbum.
-	if err := c.BindJSON(&newAlbum); err != nil {
+	// Loop over the list of albums, looking for
+	// an album whose ID value matches the parameter.
+	for _, a := range albums {
+		if a.ID == id {
+			response := APIResponse{
+				Success: true,
+				Data:    a,
+			}
+			c.JSON(http.StatusOK, response)
+			return
+		}
+	}
+
+	response := APIResponse{
+		Success: false,
+		Error:   "Album not found",
+	}
+	c.JSON(http.StatusNotFound, response)
+}
+
+// PostAlbums adds an album from JSON received in the request body.
+func PostAlbums(c *gin.Context) {
+	var newAlbum Album
+
+	// Call ShouldBindJSON to bind the received JSON to newAlbum.
+	if err := c.ShouldBindJSON(&newAlbum); err != nil {
+		response := APIResponse{
+			Success: false,
+			Error:   err.Error(),
+		}
+		c.JSON(http.StatusBadRequest, response)
 		return
 	}
 
 	// Add the new album to the slice.
 	albums = append(albums, newAlbum)
-	c.IndentedJSON(http.StatusCreated, newAlbum)
+	response := APIResponse{
+		Success: true,
+		Data:    newAlbum,
+	}
+	c.JSON(http.StatusCreated, response)
 }
 
-// getAlbumByID locates the album whose ID value matches the id
-// parameter sent by the client, then returns that album as a response.
-func getAlbumByID(c *gin.Context) {
-    id := c.Param("id")
+// UpdateAlbum updates an existing album with new data.
+func UpdateAlbum(c *gin.Context) {
+	id := c.Param("id")
 
-    // Loop over the list of albums, looking for
-    // an album whose ID value matches the parameter.
-    for _, a := range albums {
-        if a.ID == id {
-            c.IndentedJSON(http.StatusOK, a)
-            return
-        }
-    }
-    c.IndentedJSON(http.StatusNotFound, gin.H{"message": "album not found"})
+	var updatedAlbum Album
+	if err := c.ShouldBindJSON(&updatedAlbum); err != nil {
+		response := APIResponse{
+			Success: false,
+			Error:   err.Error(),
+		}
+		c.JSON(http.StatusBadRequest, response)
+		return
+	}
+
+	// Find the album in the slice and update it.
+	for i, a := range albums {
+		if a.ID == id {
+			albums[i] = updatedAlbum
+			response := APIResponse{
+				Success: true,
+				Data:    updatedAlbum,
+			}
+			c.JSON(http.StatusOK, response)
+			return
+		}
+	}
+
+	response := APIResponse{
+		Success: false,
+		Error:   "Album not found",
+	}
+	c.JSON(http.StatusNotFound, response)
+}
+
+// DeleteAlbum deletes an album with the given ID.
+func DeleteAlbum(c *gin.Context) {
+	id := c.Param("id")
+
+	// Find the album in the slice and remove it.
+	for i, a := range albums {
+		if a.ID == id {
+			albums = append(albums[:i], albums[i+1:]...)
+			response := APIResponse{
+				Success: true,
+				Data:    nil,
+			}
+			c.JSON(http.StatusOK, response)
+			return
+		}
+	}
+
+	response := APIResponse{
+		Success: false,
+		Error:   "Album not found",
+	}
+	c.JSON(http.StatusNotFound, response)
 }
 
 func main() {
-    router := gin.Default()
-    router.GET("/albums", getAlbums)
-    router.GET("/albums/:id", getAlbumByID)
-    router.POST("/albums", postAlbums)
+	router := gin.Default()
+	router.GET("/albums", GetAlbums)
+	router.GET("/albums/:id", GetAlbumByID)
+	router.POST("/albums", PostAlbums)
+	router.PUT("/albums/:id", UpdateAlbum)
+	router.DELETE("/albums/:id", DeleteAlbum)
 
-    router.Run("localhost:8085")
+	router.Run("localhost:8085")
 }
